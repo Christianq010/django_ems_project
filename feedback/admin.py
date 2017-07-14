ebb3830cb4d6911e4e055a5369dc36349a63b624
Christiaan Quyn <christianq010@gmail.com>
2017-07-14 16:22:14 +0530

    refactor: Add read_only fields form when request to edit , add seperate form when new field is added .. change placement of buttons

--- feedback/admin.py
+++ feedback/admin.py
@@ -2,10 +2,21 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django import forms
 from .models import Feedback
+from django.db import models
+
+# Our new form
+class FeedbackAddForm(forms.ModelForm):
+    comment = forms.CharField(widget=forms.Textarea(attrs={'rows':6,'cols': 40,'style': 'resize:none;'}))    
+    
+    class Meta:
+        model = Feedback
+        fields = ('name', 'subject', 'category', 'email', 'comment', 'is_read',)
 
 
+# Our form to edit
 class FeedbackForm(forms.ModelForm):
     comment = forms.CharField(widget=forms.Textarea(attrs={'rows':6,'cols': 40,'style': 'resize:none;'}))
+    created_on = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'size': '38'}))
 
     class Meta:
         model = Feedback
@@ -17,5 +28,26 @@ class FeedbackAdmin(ModelAdmin):
     search_fields = ('name', 'category', 'email', 'subject')
     list_display = ('name', 'category', 'email', 'subject', 'is_read')
     list_editable = ('is_read',)
+    # readonly_fields = ('created_on',)
+    # button placement
+    # save_on_top = True
+    # actions_on_bottom = False
+
+# below to place standard size for all text-areas etc
+    """
+    formfield_overrides = {
+    models.CharField{widget=forms.Textarea(attrs={'rows':6,'cols': 40,'style': 'resize:none;'})}
+    models.CharField{widget=forms.TextInput(attrs={'readonly': 'readonly', 'size': '38'})}
+    }
+    """
+
+# This function returns original form when a new form is requested
+# Returns another when requesting for a edit for a form that exists 
+    def get_form(self, request, obj=None, **kwargs):
+        
+        if obj is None:
+            return FeedbackAddForm
+        else: 
+            return super(FeedbackAdmin, self).get_form(request, obj, **kwargs)
 
 admin.site.register(Feedback, FeedbackAdmin)
